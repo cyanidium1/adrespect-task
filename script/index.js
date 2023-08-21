c6b8003ea34a1968cd5864bd7048d1ddf7129b7a
Fedir <99473719+cyanidium1@users.noreply.github.com>
2023-08-22 00:27:04 +0200

    added search

--- script/index.js
+++ script/index.js
@@ -19,3 +19,21 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
+
+// search
+
+const backdrop = document.querySelector("#backdrop");
+const search = document.querySelector("#search");
+const searchBtn = document.querySelector("#searchBtn");
+const openSearch = document.querySelector("#openSearch");
+const openSearchMob = document.querySelector("#openSearchMob");
+
+searchBtn.addEventListener("click", toggleBackdrop);
+openSearch.addEventListener("click", toggleBackdrop);
+openSearchMob.addEventListener("click", toggleBackdrop);
+
+function toggleBackdrop() {
+  backdrop.style.display === "none"
+    ? (backdrop.style.display = "block")
+    : (backdrop.style.display = "none");
+}
